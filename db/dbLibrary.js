a950b67f4779586574d52d13a1b14a735a5708f1
tonyvazgar <luis.vazquezga@udlap.mx>
2019-03-17 01:05:44 -0600

    libreria

--- db/dbLibrary.js
+++ db/dbLibrary.js
@@ -1,4 +1,5 @@
 const mysqlImporter = require('mysql-import');
+const mysql = require('mysql');
 
 /*
  *Para importar el archivo db.sql en mySQL a traves de MAMP
@@ -12,10 +13,44 @@ var importer = mysqlImporter.config({
     socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock', //for mac and linux
     onerror: err => console.log(err.message)
 });
-
-
-exports.createDB = function(){
+exports.createDB = function () {
     importer.import('db.sql').then(() => {
         console.log("La base de datos ha sido importada! :)");
+    });
+}
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: '8889',
+    user: 'root',
+    password: 'root',
+    database: 'loginSystem'
+});
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected!');
+});
+
+/*
+ *Seleccionar todos los usuarios
+ */
+exports.selectAllFrom = function (table) {
+    connection.query("SELECT * FROM " + table, (err, rows) => {
+        if(err) throw err;
+        console.log('Datos recividos para la query de ' + table +  ':\n');
+        console.log(rows);
+    });
+}
+
+/*
+ *Seleccionar usuario especifico
+ */
+exports.selectUser = function(user) {
+    var query = "SELECT * FROM users WHERE user_id = '" + user + "'";
+    console.log(query);
+    connection.query( query, (err, rows) => {
+        if(err) throw err;
+        console.log("Datos de " + user + ":\n");
+        console.log(rows);
     });
 }
--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-var db  = require('./db/dbLibrary.js');
+const express = require('express');
+const app = express()
+const db  = require('./db/dbLibrary.js');
 
 db.createDB();
+db.selectUser("tony");
+db.selectAllFrom("users");
+db.selectAllFrom("rol");
+db.selectAllFrom("permission");
+db.selectUser("root");
+
+app.listen(3000, () => {
+    console.log('App running on port 3000')
+})
