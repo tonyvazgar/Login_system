aac7bb17b6819d75acd549f2d5fc2f4a752d53b2
tonyvazgar <luis.vazquezga@udlap.mx>
2019-03-16 23:51:32 -0600

    Creacion automatica de base de datos

--- db.sql
+++ db.sql
@@ -1,5 +1,7 @@
-CREATE TABLE users ( user_id SERIAL PRIMARY KEY, password VARCHAR(50) NOT NULL );
-
+CREATE DATABASE loginSystem;
+USE loginSystem;
+CREATE TABLE users ( user_id VARCHAR(50), pass VARCHAR(50) NOT NULL );
+INSERT INTO users (user_id, pass) VALUES ('tony', '1234');
+INSERT INTO users (user_id, pass) VALUES ('root', 'root');
 CREATE TABLE rol ( rol_id serial PRIMARY KEY);
-
 CREATE TABLE permission (perm_id serial PRIMARY KEY, p_str VARCHAR(50), type VARCHAR(50) );
