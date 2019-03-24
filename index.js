f6ba1108721ae77c2f2cfe5e30ce5a3e452ac0bc
tonyvazgar <luis.vazquezga@udlap.mx>
2019-03-24 14:44:16 -0600

    :(

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var app = express()
-var db = require('./db/dbLibrary.js');
-var bodyParser = require('body-parser');
-var path = require('path');
+var express     = require('express');
+var db          = require('./db/dbLibrary.js');
+var bodyParser  = require('body-parser');
+var path        = require('path');
+var session     = require('express-session');
+var app         = express();
 
 // db.createDB();
 // db.selectUser("tony");
@@ -15,32 +16,57 @@ var path = require('path');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(session({
+    secret: 'millave',
+    resave: true,
+    saveUninitialized: true
+}));
+
 app.get('/', function (request, response) {
     response.sendFile(path.join(__dirname + '/public/login.html'));
 });
 app.use(express.static(path.resolve('./public')));
 
 app.get('/home', function (request, response) {
-    response.sendFile(path.join(__dirname + '/public/home.html'));
+
+    /*
+     
+    
+    
+     * ARRELGAR ESTOS IFS!!!!!
+
+
+
+     */
+    if(request.session.loggedin){
+        response.send('/public/home.html');
+    }else{
+        response.send('/public/login.html');
+    }
+    response.end();
 });
 
 app.post('/log', function (request, response) {
     console.error("\nHaciendo post!!\n");
     var username = request.body.username;
     var password = request.body.password;
-    console.log(username + "--" + password);
-
     if (username && password) {
         db.connection.query('SELECT * FROM users WHERE user_id = ? AND pass = ?', [username, password], function (error, results, fields) {
-            console.log(results);
+            console.log("Los resultados de la query son: ", results);
             if (results.length > 0) {
+                console.log("Usuario logeado con exito!");
+                request.session.loggedin = true;
+                request.session.username = username;
                 response.redirect('/home');
             } else {
-                //response.send('Usuario y contraseña erroneos!');
+                console.log("Error en contraseña, intenta de nuez");
                 response.redirect('/flog');
             }
             response.end();
         });
+    } else {
+        console.log('Ingresa tus datos!!!');
+        response.redirect('/flog');
     }
 });
 
